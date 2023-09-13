1d500145b9eaaa794fa74e9c070897b39e62955a
卢天宇 <1141049271@qq.com>
2023-09-13 23:03:44 +0800

    feat: 测试axios

--- src/views/AsyncFetch/index.tsx
+++ src/views/AsyncFetch/index.tsx
@@ -7,13 +7,39 @@ import React from 'react'
 import { Button, Space } from '@arco-design/web-react';
 import store from '@/store';
 import { fetchData } from '@/store/modules/users';
+import Axios, { AxiosInstance } from 'axios';
+
+const axios: AxiosInstance = Axios.create({
+  baseURL: 'https://randomuser.me'
+})
 
 export default function AsyncFetch() {
+
+  function axiosGetHandler() {
+    axios.get('/api').then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+  function axiosPostHandler() {
+    axios.post('http://localhost:3000/api').then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
+  }
   return (
     <div>AsyncFetch
-      <Button onClick={() => {
-        store.dispatch(fetchData());
-      }}>请求</Button>
+      <div>
+        <Space>
+          <Button onClick={() => {
+            store.dispatch(fetchData());
+          }}>请求</Button>
+          <Button onClick={axiosGetHandler}>请求Axios---Get</Button>
+          <Button onClick={axiosPostHandler}>请求Axios---Post</Button>
+        </Space>
+      </div>
     </div>
   )
 }
